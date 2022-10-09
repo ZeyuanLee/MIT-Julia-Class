f8aca3a88c2d3483ce0d652c2d3d117f08557979
Zeyuan <773594195@qq.com>
2022-10-09 17:45:43 +0800

    First commit.

---
+++ Section-1.jl
@@ -0,0 +1,7 @@
+dump(1:10)
+#dump(x; maxdepth=8)
+#Show every part of the representation of a value. The depth of the output is truncated at maxdepth.
+collect(1:10)
+
+using Colors
+RGB(0.5,0.4,0.3)
